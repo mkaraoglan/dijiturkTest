65cd93211183510bc4173f935382eec51b9afcdc
Mücahit Karaoğlan <mucahitkaraoglan@gmail.com>
2020-03-15 21:52:41 +0300

    1

--- test/src/main/java/dijiturk/test/App.java
+++ test/src/main/java/dijiturk/test/App.java
@@ -11,6 +11,7 @@ public class App
     public static void main( String[] args ) throws IOException
     {
     	System.out.println( "s World!" );
-    	Base.initializeDriver();
+    	Base base = new Base();
+    	base.initializeDriver();
     }
 }
--- test/src/main/java/dijiturk/test/Base.java
+++ test/src/main/java/dijiturk/test/Base.java
@@ -14,29 +14,14 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Base {
-	public static WebDriver driver;
-	public static WebDriverWait wait;
+	public WebDriver driver;
+	public WebDriverWait wait;
 	
-	public static void connectURL(WebDriver driver, String URL) {
+	private void connectURL(WebDriver driver, String URL) {
 		driver.get(URL);
 	}
 	
-	//initialize driver
-	public static void initializeDriver() throws IOException {
-		
-		Properties prop = new Properties();
-		FileInputStream fs = new FileInputStream("/test/src/main/java/resources/data.properties");
-		prop.load(fs);
-		String browserName = prop.getProperty("browser");
-		
-		if(browserName.equalsIgnoreCase("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "/test/driver/chromedriver.exe");
-			driver = new ChromeDriver();
-		}
-
-		driver.manage().window().maximize();
-		
-		connectURL(driver, prop.getProperty("url"));
+	private void subscribe (WebDriver driver) {
 		waitForLoading();
 
 		int currentWindowWidth = driver.manage().window().getSize().getWidth();
@@ -50,26 +35,30 @@ public class Base {
 			driver.findElement(By.xpath("//*[@id=\"navbarCollapse\"]/ul[1]/li/a")).click();
 		}
 
+	}
+	
+	private void register(WebDriver driver) {
 		waitForLoading();
 		
 		driver.findElement(By.cssSelector("body > div.layout-wide > div.container-onboarding.step1 > div:nth-child(4)")).click();
 
 		driver.findElement(By.cssSelector("a[data-btn-index='1']")).click();
 		
+	}
+	
+	private void createAccaunt(WebDriver driver, String firstName, String lastName, String emailOrPhone, String password) {
+		
 		waitForUrlChange();
 		waitForLoading();
-
-		String firstName = prop.getProperty("firstName");
-		String lastName = prop.getProperty("lastName");
-		String emailOrPhone = prop.getProperty("emailOrPhone");
-		String password = prop.getProperty("password");
 		WebElement formRegister = driver.findElement(By.cssSelector("#form-register"));
 		formRegister.findElement(By.cssSelector("input[name='FirstName']")).sendKeys(firstName);
 		formRegister.findElement(By.cssSelector("input[name='LastName']")).sendKeys(lastName);
 		formRegister.findElement(By.cssSelector("input[name='EmailOrPhone']")).sendKeys(emailOrPhone);
 		formRegister.findElement(By.cssSelector("input[name='Password']")).sendKeys(password);
 		formRegister.findElement(By.cssSelector("input[type='submit']")).click();
-		
+	}
+	
+	private void ignoreEmailVerification(WebDriver driver) {
 		waitForUrlChange();
 		waitForLoading();
 		
@@ -79,13 +68,12 @@ public class Base {
 
 		driver.findElement(By.cssSelector("input[type='submit'][name='pay-now']")).click();
 		
+	}
 	
-		waitForUrlChange();
-		String totalCharge = driver.findElement(By.xpath("//*[@id=\"ncol_ref\"]/tbody/tr[2]/td[2]/small")).getText();
-		System.out.println(totalCharge);
+	private void makePayment(WebDriver driver, String firstName, String cardNumber) {
+
 		driver.findElement(By.id("Ecom_Payment_Card_Name")).sendKeys(firstName);
 		
-		String cardNumber = prop.getProperty("cardNumber");
 		driver.findElement(By.id("Ecom_Payment_Card_Number")).sendKeys(cardNumber);
 		
 		driver.findElement(By.id("Ecom_Payment_Card_ExpDate_Month")).click();
@@ -96,21 +84,58 @@ public class Base {
 		
 		driver.findElement(By.id("Ecom_Payment_Card_Verification")).sendKeys("123");
 		
-
 		driver.findElement(By.id("submit3")).click();
 
+	}
+	//initialize driver
+	public void initializeDriver() throws IOException {
+		
+		Properties prop = new Properties();
+		FileInputStream fs =  new FileInputStream("C:\\Users\\MK\\git\\dijiturkTest\\test\\src\\main\\java\\resources\\data.properties");
+		System.out.println(fs);
+		prop.load(fs);
+		String browserName = prop.getProperty("browser");
+		
+		if(browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\MK\\git\\dijiturkTest\\test\\driver\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+
+		driver.manage().window().maximize();
+		
+		connectURL(driver, prop.getProperty("url"));
+		
+		subscribe(driver);
+		
+		register(driver);
+
+		String firstName = prop.getProperty("firstName");
+		String lastName = prop.getProperty("lastName");
+		String emailOrPhone = prop.getProperty("emailOrPhone");
+		String password = prop.getProperty("password");
+		createAccaunt(driver, firstName, lastName, emailOrPhone, password);
+		
+		ignoreEmailVerification(driver);
+
+		
+		//test here
+		waitForUrlChange();
+		String totalCharge = driver.findElement(By.xpath("//*[@id=\"ncol_ref\"]/tbody/tr[2]/td[2]/small")).getText();
+		System.out.println(totalCharge);
+		String cardNumber = prop.getProperty("cardNumber");
+		makePayment(driver, firstName, cardNumber);
+		
 		String result = driver.findElement(By.cssSelector("#content > div > table:nth-child(6) > tbody > tr > td > h3")).getText();
 		System.out.println(result);
-		System.out.println("yüklendi");
 	}
 
 	
-	public static void waitForLoading() {
+	public void waitForLoading() {
 		wait = new WebDriverWait(driver, 10);
 		wait.until(ExpectedConditions.attributeToBe(By.id("loading"), "style", "display: none;"));
 	}
 	
-	public static void waitForUrlChange() {
+	public void waitForUrlChange() {
 		wait.until(ExpectedConditions.not(ExpectedConditions.urlToBe(driver.getCurrentUrl())));
 	}
     
